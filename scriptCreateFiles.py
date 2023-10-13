4eb734e6660a644c813884bb09a80eed7a54749f
Caio Dantas <caiodantasl.lara@gmail.com>
2023-10-13 10:21:10 -0300

    create: files my answers

--- scriptCreateFiles.py
+++ scriptCreateFiles.py
@@ -4,7 +4,7 @@ FILE_DEST = Path(
     'C:\\Users\\caio.dantas\\Desktop\\RBook-Computer-Networking-Kurose\\fixation_exercises\\ch_01')
 
 
-for i in range(3, 6):
+for i in range(3, 21):
     name_file = FILE_DEST / f"e_{i:03d}.md"
     with open(name_file, 'a') as file:
         lines = ['###\n', '\n', '#\n']
