e3db971ba19df6bead804e308136586e511e0a90
daantascaio <caiodantasl.lara@gmail.com>
2025-05-14 13:20:06 -0300

    criando os arquivos futuros

---
+++ fixation_exercises/ch_02_application_layer/problems_fix_ex/script_2.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+FILE_DEST = Path(__file__).parent
+
+
+for i in range(1, 32):
+    name_file = FILE_DEST / f"p_{i:03d}.md"
+    with open(name_file, 'a') as file:
+        lines = ['###\n', '\n', '#\n']
+        file.writelines(lines)
